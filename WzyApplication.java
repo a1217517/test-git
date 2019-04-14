145d3df24731f387168705e83949b67d4e91533c
wanzeyu <1192640540@qq.com>
2019-04-14 17:03:43 +0800

    第一次提交

---
+++ WzyApplication.java
@@ -0,0 +1,13 @@
+package com.testgit.wzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WzyApplication.class, args);
+    }
+
+}
